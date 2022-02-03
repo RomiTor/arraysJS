0db109a2957e5bb61fabdea3172b26c42ed49645
Romina Torres Barahona <rominatorresbarahona@MacBook-Pro-de-Romina.local>
2022-02-03 14:24:51 -0300

    cambios ejercicio 12

--- ejercicio12.js
+++ ejercicio12.js
@@ -1,6 +1,10 @@
 const move = (someArray, firstIndex, secondIndex) => {
+    const firstItem = someArray[firstIndex]
+    const secondItem = someArray[secondIndex]
+    someArray[secondIndex] = firstItem
+    someArray[firstIndex] = secondItem
     
-    
+    return someArray
 }
 
 console.log(move([4, 5, 7], 2, 1)) // [4, 7, 5]
